0023daeb37d68300fab27468027191cddcb9155c
Darron Froese <darron@froese.org>
2014-02-19 15:18:36 -0700

    First commit.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+name             'proftpd'
+maintainer       'Darron Froese'
+maintainer_email 'darron@froese.org'
+license          'Apache 2.0'
+description      'Installs/configures something'
+version          '0.1.0'
+recipe           'proftpd::default', 'Installs/configures something'
